99d273481c5379c58b1e7eed4e8a2a3208f413d7
izumi <kizumirazuki@gmail.com>
2023-12-07 01:57:46 +0700

    Update Base Application : Api Configuration & Repository + ViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-parcelize")
+    id("com.google.devtools.ksp") 
 }
 
 android {
@@ -65,6 +67,17 @@ dependencies {
 //  Datastore Preference
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.activity:activity-ktx:1.8.1")
+
+//    Room
+    val room_version = "2.6.1"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
+    ksp("androidx.room:room-compiler:$room_version")
+
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
 
 //  Glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
