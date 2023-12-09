ba58b34893c6c320404bcf07f43bc2d3b8118a44
Fawwaz Anugrah Wiradhika Dharmasatya <anugrahdwfawwaz@gmail.com>
2023-12-09 10:44:11 +0700

    add premilinary endpoint for update profile

--- src/controller/ProfileController.ts
+++ src/controller/ProfileController.ts
@@ -1,20 +1,25 @@
-import { Request,Response,NextFunction } from "express";
-import BaseController from "./BaseController";
-import logger from "../utils/logger";
-import firestoreClient from "../utils/firestoreClient";
-import User from "../interfaces/users";
+import { Request, Response, NextFunction } from 'express'
+import BaseController from './BaseController'
+import logger from '../utils/logger'
+import firestoreClient from '../utils/firestoreClient'
+import User from '../interfaces/users'
+import { Filter, Timestamp } from '@google-cloud/firestore'
+import * as bcrypt from 'bcrypt'
 
-class ProfileController extends BaseController{
-  getProfile = async (req:Request,res:Response,next:NextFunction) => {
-    try{
-      const {id} = req.params
+class ProfileController extends BaseController {
+  getProfile = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params
       logger.info(`Profile Request For User ${id}`)
-      const usersSnapshot = await firestoreClient.collection('users').doc(id).get()
+      const usersSnapshot = await firestoreClient
+        .collection('users')
+        .doc(id)
+        .get()
       const user: User = {
         id: usersSnapshot.id,
-        data: usersSnapshot.data() as any,
-      };
-      const data =  {
+        data: usersSnapshot.data() as any
+      }
+      const data = {
         username: user.data.username,
         first_name: user.data.first_name,
         last_name: user.data.last_name,
@@ -23,10 +28,81 @@ class ProfileController extends BaseController{
         weight: user.data.weight
       }
       res.json({
-        data,       
-        message: "User profile retrieved"
+        data,
+        message: 'User profile retrieved'
       })
-    }catch(err:unknown){
+    } catch (err: unknown) {
+      logger.error(err)
+      res.status(500).send({
+        message: 'internal server error'
+      })
+    }
+  }
+
+  updateProfile = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params
+      const data = req.body
+      const userRef = await firestoreClient.collection('users').doc(id)
+      let usersSnapshot: FirebaseFirestore.DocumentSnapshot<
+        FirebaseFirestore.DocumentData,
+        FirebaseFirestore.DocumentData
+      >
+      let existingData: User | undefined
+      //proses data yang sensitif
+      if (data.date_of_birth) {
+        data.date_of_birth = Timestamp.fromDate(new Date(data.date_of_birth))
+      }
+      //fetch data yang sudah ada bila perlu aja
+      if (data.username || data.email || data.password) {
+        usersSnapshot = await userRef.get()
+        existingData = usersSnapshot.data() as User
+      }
+      if (data.username || data.email) {
+        //pastiin unik
+        const existingUser = await firestoreClient
+          .collection('users')
+          .where(
+            Filter.or(
+              Filter.where(
+                'username',
+                '==',
+                data.username ?? existingData!!.data.username
+              ),
+              Filter.where(
+                'email',
+                '==',
+                data.email ?? existingData!!.data.email
+              )
+            )
+          )
+          .get()
+        if (!existingUser.empty) {
+          //kalau dah ada, gak bisa register pakai username ini
+          res.status(409).send({
+            message: `username or email already exist`
+          })
+          return
+        }
+      }
+      //konversi ke angka
+      if (data.weight) {
+        data.weight = Number(data.weight)
+      }
+      if (data.height) {
+        data.height = Number(data.height)
+      }
+      //hash password
+      if (data.password) {
+        data.password = await bcrypt.hash(
+          data.password,
+          existingData!!.data.salt
+        )
+      }
+      await userRef.update(data)
+      logger.info(`Profile Modification Request For User ${id}`)
+      res.send({ message: 'Profile updated' })
+    } catch (err: unknown) {
       logger.error(err)
       res.status(500).send({
         message: 'internal server error'
@@ -35,4 +111,4 @@ class ProfileController extends BaseController{
   }
 }
 
-export default new ProfileController();
+export default new ProfileController()
--- src/routes/ProfileRoutes.ts
+++ src/routes/ProfileRoutes.ts
@@ -5,6 +5,7 @@ import BaseRoutes from "./BaseRoutes";
 class ProfileRoutes extends BaseRoutes{
   public setRoutes(): void {
       this.routes.get('/:id',[JwtPathMiddleware],ProfileController.getProfile)
+      this.routes.post('/:id',[JwtPathMiddleware],ProfileController.updateProfile)
   }
 }
 export default new ProfileRoutes().routes
