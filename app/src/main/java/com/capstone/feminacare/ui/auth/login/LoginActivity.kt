1c71c21e8942ff69622e5d34ff7e5c7210bdda26
wilelilac <wildfahreza@gmail.com>
2023-12-13 23:23:19 +0700

    login register activity

---
+++ app/src/main/java/com/capstone/feminacare/ui/auth/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.feminacare.ui.auth.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.feminacare.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
