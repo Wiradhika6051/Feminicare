33da3243e4e7b2697a6a150a3defa95fedd663a4
Christine <christinehutabarat12@gmail.com>
2023-12-17 18:23:18 +0700

    Update constant.ts

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -2,8 +2,10 @@ import "dotenv/config";
 
 export const CONST = {
   PORT: process.env.port ?? 8080,
-  PROJECT_ID:process.env.PROJECT_ID,
-  ENVIRONMENT:process.env.ENVIRONMENT,
-  KEY_PATH:process.env.KEY_PATH,
-  SECRET:process.env.SECRET,
-}
+  PROJECT_ID: process.env.PROJECT_ID,
+  ENVIRONMENT: process.env.ENVIRONMENT,
+  KEY_PATH: process.env.KEY_PATH,
+  SECRET: process.env.SECRET,
+  BUCKET_CREDENTIALS: process.env.BUCKET_CREDENTIALS,
+  ML_BUCKET_NAME: process.env.ML_BUCKET_NAME,
+};
